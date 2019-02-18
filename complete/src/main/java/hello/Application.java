cd17d07a30718617239f600ace237a27ac941290
mheck <mark.heck@ericsson.com>
2019-02-18 11:32:35 -0500

    Pipeline tutorial From https://docs.microsoft.com/en-us/azure/jenkins/tutorial-jenkins-deploy-web-app-azure-app-service#create-build-and-deployment-files

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -20,6 +20,8 @@ public class Application {
         return args -> {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
+            System.out.println("Let's inspect the beans provided by Spring Boot on Azure");
+
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
